94a348c6a7118ecb0c8f8c2d577598f19595f250
mohrezk <111631776+mohrezk@users.noreply.github.com>
2024-02-17 23:03:29 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    # "django.middleware.csrf.CsrfViewMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
@@ -173,6 +173,11 @@ CORS_ALLOWED_ORIGINS = [
     'http://*',
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://geodjango-test.onrender.com',
+    'https://*', 
+    'http://*',
+]
 
 
 # GDAL_LIBRARY_PATH = r"C:\OSGeo4W\bin\gdal308.dll"
