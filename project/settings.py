79aa7171ac00329ec84064b7ef53122da8125b98
mohrezk <111631776+mohrezk@users.noreply.github.com>
2024-02-17 22:52:08 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -27,8 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_COOKIE_SECURE = False  
-CSRF_COOKIE_HTTPONLY = False
+# CSRF_COOKIE_SECURE = False  
+# CSRF_COOKIE_HTTPONLY = False
 
 # Application definition
 
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    # "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
