90a3b6e9b1fbdfd389f34308e3f4e8ba5f4dcf8c
mohrezk <111631776+mohrezk@users.noreply.github.com>
2024-02-17 22:14:21 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,10 @@ echo "Applying database migrations..."
 python manage.py makemigrations
 python manage.py migrate
 
+
+echo "Creating superuser..."
+echo "from users.models import User; User.objects.create_superuser('admin', 'admin@exmaple.com', '12345')" | python manage.py shell
+
 # Start the Django development server
 echo "Starting Django development server..."
 python manage.py runserver 0.0.0.0:8000
