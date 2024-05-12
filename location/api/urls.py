e3d29106d6b8820ebb867e32ceefa175e9023822
Mohamed Shehata <80494827+shehata18@users.noreply.github.com>
2024-05-12 22:14:23 +0300

    Update urls.py

--- location/api/urls.py
+++ location/api/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path("set-location/", LocationView.as_view()),
-    path("get-nearby/", NearbyUsers.as_view()),
+    # path("get-nearby/", NearbyUsers.as_view()),
+    path("get-all-nearby/", NearbyServiceProvidersAndUsers.as_view()),
                 
 ]
