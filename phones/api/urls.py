ca193fb5aa7254161c8df61a1a39646a65ffada1
Mohamed Shehata <80494827+shehata18@users.noreply.github.com>
2024-06-23 18:58:00 +0300

    Update urls.py sinch endpoint

--- phones/api/urls.py
+++ phones/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import RequestPhoneVerificationTwilioView, VerifyPhoneNumberTwilioView,RequestPhoneVerificationSinchView
+from .views import RequestPhoneVerificationTwilioView, VerifyPhoneNumberTwilioView,RequestPhoneVerificationSinchView,VerifyPhoneNumberSinchView
 urlpatterns = [
     path('request-phone-verification-twilio/', RequestPhoneVerificationTwilioView.as_view(), name='request-phone-verification'),
     path('verify-phone-number-Twilio/', VerifyPhoneNumberTwilioView.as_view(), name='verify-phone-number'),
 
-    path('request-phone-verification-sinch/', RequestPhoneVerificationSinchView.as_view(), name='request-phone-verification'),
-    # path('verify-phone-number/', VerifyPhoneNumberSinchView.as_view(), name='verify-phone-number'),
+    path('request-phone-verification/', RequestPhoneVerificationSinchView.as_view(), name='request-phone-verification'),
+    path('verify-phone-number/', VerifyPhoneNumberSinchView.as_view(), name='verify-phone-number'),
 ]
