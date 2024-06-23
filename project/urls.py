5ad9c5abad037e7fef7f98da5b91f7e5f4d4c757
mohrezk <111631776+mohrezk@users.noreply.github.com>
2024-06-23 17:43:53 +0300

    Update urls.py
    add phones urls

--- project/urls.py
+++ project/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("users.api.urls")),
     path('api/', include("location.api.urls")),
+    path('api/', include("phones.api.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
